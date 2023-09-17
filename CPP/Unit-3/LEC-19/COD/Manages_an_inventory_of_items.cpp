204ce841228b89d4954a0ad65ac14301d4687bea
Veera <119814697+veerababu2103@users.noreply.github.com>
2023-09-17 23:58:35 +0530

    Add files via upload

---
+++ CPP/Unit-3/LEC-19/COD/Manages_an_inventory_of_items.cpp
@@ -0,0 +1,22 @@
+// manages an inventory of items
+
+#include <iostream>
+using namespace std;
+
+class wl{};
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    string item[n];
+    int num[n];
+    float cost[n];
+
+    for(int i=0;i<n;i++){
+        
+    }
+
+    return 0;
+}
